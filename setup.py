24e810fc6f3ef8131f7cad2b6f38413c64691af8
Dimitris Papagiannis <d.papag.a@gmail.com>
2025-03-20 15:02:41 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -258,7 +258,7 @@ class InstallCommand(install):
 
 setup(
     name="dqmgui",
-    version="10.3.0",
+    version="10.3.1",
     maintainer_email="hn-cms-dqmDevel@cern.ch",
     cmdclass={"build_system": BuildCommand, "install_system": InstallCommand},
     package_dir={"Monitoring": "src/python"},
